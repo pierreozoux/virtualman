c5ab40f54c86e3435380742cb085a2a82923b30e
pierre-o13 <pierre.ozoux@seedrs.com>
2012-09-11 10:46:18 +0100

    cooking is now working in interractive mode

--- lib/virtualman/interractive.rb
+++ lib/virtualman/interractive.rb
@@ -1,23 +1,24 @@
 #require 'lib/virtualman'
 
-def choosed_vm(array,vm_name)
-	array.each_with_index do |vm,index|
-		if vm["name"] == vm_name
+def choosed_option(array,choice,attribute_to_check)
+	array.each_with_index do |option,index|
+		if option[attribute_to_check] == choice
 			return array[index]
 		end
 	end
 end
 
-def choose options, *attribute_to_print
+def choose options, *attribute_to_check
 	options_array = load_conf[options]
-	if attribute_to_print.empty?
+	if attribute_to_check.empty?
 		return Menu.unic_run(options_array)
 	else
 		list = Array.new
-		options_array.each do |vm|
-			list << vm[attribute_to_print]
+		options_array.each do |option|
+			list << option[attribute_to_check[0]]
 		end
-		return Menu.unic_run(list)
+		choice = Menu.unic_run(list)
+		return choosed_option(options_array,choice,attribute_to_check[0])
 	end
 end
 
@@ -25,15 +26,13 @@ def clone_vm
 	puts "----Clone_VM----"
 	puts "Which VM do you want to clone?"
 
-	vm_choice = choose "source_vms" "name"
+	vm_choice = choose "source_vms", "name"
+	vm_to_clone = Vm.new("\"#{vm_choice["name"]}\"")
 
 	puts "How do you want to name your freshly spawned VM?"
 	vm_name = Menu.ask
 
-	vm_to_clone_info = choosed_vm(source_vms,vm_choice)
-
-	vm_to_clone = Vm.new("\"#{vm_to_clone_info["name"]}\"")
-	options = "--snapshot '#{vm_to_clone_info["snapshot"]}' --name #{vm_name} --register"
+	options = "--snapshot '#{vm_choice["snapshot"]}' --name #{vm_name} --register"
 	vm_to_clone.manage("clonevm", options)
 
 	vm_cloned = Vm.new("\"#{vm_name}\"")
@@ -81,17 +80,22 @@ end
 def cook
 	puts "----Cook a clone!----"
 	puts "Which VM do you want to cook?"
+	role_path = load_conf["role_path"]
+	cookbook_path = load_conf["cookbook_path"]
 
-	vm_name = choose "cloned_vms" "name"
+	vm_choice = choose "cloned_vms", "name"
 
-	selected_vm = Vm.new(vm_name)
+	selected_vm = Vm.new(vm_choice["name"])
 
 	puts "Which role do you want to give?"
 
 	role = choose "roles"
 
+	user="root"
+
 	if start_vm(selected_vm)
-		cmd = "ssh #{user}@#{selected_vm.ip}"
+		cmd = "ssh #{user}@#{selected_vm.ip} '/usr/local/bin/chef-solo -j #{role_path}#{role}.json -r #{cookbook_path}'"
+		p cmd
 		`#{cmd}`
 	end
 
@@ -102,9 +106,9 @@ def ssh_cloned
 	puts "----SSH to cloned VM----"
 	puts "Which VM do you want to ssh?"
 
-	vm_name = choose "cloned_vms" "name"
+	vm_choice = choose "cloned_vms", "name"
 
-	selected_vm = Vm.new(choice)
+	selected_vm = Vm.new(vm_choice["name"])
 
 	puts "With which user? [root]"
 	user = Menu.ask
